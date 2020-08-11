6d6abfe62860c03647dad0b9f470e12f66834fd5
lrsilva81 <leo.ribeiro.silva@gmail.com>
2020-08-11 23:00:56 +0100

    Backend integration

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,56 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string,
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/50154845?s=460&u=72f86715f6d9ae21dbc4a1d4b9260d551151823f&v=4" alt="Leonardo Silva"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Leonardo Silva</strong>
-                    <span>Ciências da Computação</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Consectetur amet ipsum ad culpa.
-                <br/><br/>
-                Culpa elit culpa consequat magna cupidatat duis est. In ut aliqua excepteur aute non id cillum laboris occaecat duis. Incididunt in nulla esse fugiat amet excepteur eiusmod non irure nostrud deserunt. Sunt occaecat dolor pariatur elit enim eu. Laboris anim non duis duis.
+            {teacher.bio}
             </p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    // eslint-disable-next-line react/jsx-no-target-blank
+                    target="_blank"
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
 
